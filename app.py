be9eaa28a99c6159d9b44f6ed6738aeae76e5327
farkandezakwan-sys <farkandezakwan@gmail.com>
2026-05-19 22:06:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,77 @@
 import streamlit as st
 from ultralytics import YOLO
-from PIL import Image
 import tempfile
 import os
+import glob
+import gc
+
+st.set_page_config(
+    page_title="AI Pothole Detection",
+    layout="wide"
+)
 
 st.title("AI Based Pothole Detection System")
 
-if not os.path.exists("best (2).pt"):
-    st.error("best (2).pt model file not found")
-    st.stop()
+st.write("Upload one road video")
 
 @st.cache_resource
 def load_model():
-    return YOLO("best (2).pt")
+    return YOLO("best.pt")
 
 model = load_model()
 
-uploaded = st.file_uploader(
-    "Upload Image",
-    type=["jpg","jpeg","png"]
+uploaded_file = st.file_uploader(
+    "Upload Video",
+    type=["mp4","avi","mov"]
 )
 
-if uploaded:
+if uploaded_file:
 
-    image = Image.open(uploaded)
+    st.subheader(uploaded_file.name)
 
-    with tempfile.NamedTemporaryFile(
+    temp_input = tempfile.NamedTemporaryFile(
         delete=False,
-        suffix=".jpg"
-    ) as tmp:
-
-        image.save(tmp.name)
-
-        results = model.predict(tmp.name)
-
-    result = results[0].plot()
-
-    st.image(
-        result,
-        caption="Detection Result",
-        use_container_width=True
+        suffix=".mp4"
     )
+
+    temp_input.write(
+        uploaded_file.read()
+    )
+
+    temp_input.close()
+
+    with st.spinner("Detecting potholes..."):
+
+        results = model.predict(
+            source=temp_input.name,
+            save=True,
+            imgsz=320,
+            conf=0.5
+        )
+
+    save_dir = str(results[0].save_dir)
+
+    videos = (
+        glob.glob(save_dir+"/*.mp4")
+        + glob.glob(save_dir+"/*.avi")
+    )
+
+    if videos:
+
+        output = videos[0]
+
+        st.success("Detection completed")
+
+        st.video(output)
+
+        with open(output,"rb") as f:
+
+            st.download_button(
+                "Download Result",
+                data=f,
+                file_name="detected_video.mp4"
+            )
+
+    # Cleanup memory
+    os.remove(temp_input.name)
+    gc.collect()
